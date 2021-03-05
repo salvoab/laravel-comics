dc9630cc316dcef3ed1fe44667b591e77cb3d914
salvoab <salvatore.abate85@gmail.com>
2021-03-05 13:29:13 +0100

    navbar principale

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,29 @@
                 <a href="#" class="top_link"> <img src="{{asset('img/DC_SHOP_desktop.svg')}}" alt="DC universe"> </a>
                 <a href="#" class="top_link"> <img src="{{asset('img/DC_community.svg')}}" alt="DC universe"> </a>
                 <a href="#" class="top_link"> <img src="{{asset('img/DC_on_HBOMAX_desktop.svg')}}" alt="DC universe"> </a>
-
+            </nav>
+            <nav class="main_nav">
+                <div class="container">
+                    <div class="left_nav">
+                        <img src="{{ asset('img/logo.png') }}" alt="DC Logo">
+                    </div>
+                    <div class="middle_nav">
+                        <ul>
+                            <li><a href="#">CHARACTERS</a></li>
+                            <li><a href="#">COMICS</a></li>
+                            <li><a href="#">MOVIES</a></li>
+                            <li><a href="#">TV</a></li>
+                            <li><a href="#">GAMES</a></li>
+                            <li><a href="#">VIDEOS</a></li>
+                            <li><a href="#">NEWS</a></li>
+                            <li><a href="#">SHOP</a></li>
+                        </ul>
+                    </div>
+                    <div class="right_nav">
+                        <input type="text" name="search" id="search" placeholder="Search">
+                        <img src="{{ asset('img/search-icon-new.png') }}" alt="search">
+                    </div>
+                </div>
             </nav>
         </header>
 
