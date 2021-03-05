145e007bd24933a9eda5600a1a1bcc7c386d6e8d
salvoab <salvatore.abate85@gmail.com>
2021-03-05 17:09:21 +0100

    frontend: parte alta della pagina comics completa

--- resources/views/guests/comics.blade.php
+++ resources/views/guests/comics.blade.php
@@ -3,13 +3,33 @@
 @section('content')
 
 <div class="top_comic_page">
-    @foreach($comics as $comic)
-        <div class="img_wrapper">
-            <a href="{{route('show_comic', ['comic'=> $comic->id])}}"> 
-                <img src="{{ asset('storage/' . $comic->cover) }}" alt="$comic->title">
-            </a>
+    <nav>
+        <div class="container">
+            <div class="left"> 
+                <h2>COMICS & GRAPHIC NOVELS</h2> 
+            </div>
+
+            <div class="right">
+                <a href="#" class="active">THIS WEEK</a>
+                <a href="#">LAST WEEK</a>
+                <a href="#">NEXT WEEK</a>
+                <a href="#">UPCOMING</a>
+                <a href="#">SEE ALL</a>
+            </div>
         </div>
-    @endforeach
+    </nav>
+    
+    <div class="comics">
+        @foreach($comics as $comic)
+            <div class="comic_wrapper">
+                <a href="{{route('show_comic', ['comic'=> $comic->id])}}"> 
+                    <img src="{{ asset('storage/' . $comic->cover) }}" alt="$comic->title">
+                </a>
+
+                <div class="tag">COMIC BOOK</div>
+            </div>
+        @endforeach
+    </div>
 </div>
 
 @endsection
