3a43f1842a0c6c4c1102f6bfe180f7ffc99553cd
salvoab <salvatore.abate85@gmail.com>
2021-03-07 14:27:02 +0100

    front-end: pagina show_comic parte inferiore della pagina

--- resources/views/guests/show_comic.blade.php
+++ resources/views/guests/show_comic.blade.php
@@ -34,4 +34,79 @@
     </div>
 </div>
 
+<div class="lower_comic_show_page">
+    <div class="container">
+        <div class="row">
+            <!-- Talent -->
+            <div class="col-xs-12 col-md-6 col-lg-6">
+                <div class="informations">
+                    <h4>Talent</h4>
+                    <hr>
+                    <div class="information">
+                        <dt>Art by</dt>
+                        <dd>
+                            @foreach($comic->artists as $artist)
+                                <a href="#">{{$artist->name . ' ' . $artist->lastname}}</a>
+                                @if( !$loop->last) 
+                                ,
+                                @endif
+                            @endforeach
+                        </dd>
+                    </div>
+                    <hr>
+                    <div class="information">
+                        <dt>Written by</dt>
+                        <dd><a href="#">{{$comic->author->name . ' ' . $comic->author->lastname}}</a></dd>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+
+            <!-- Specs -->
+            <div class="col-xs-12 col-md-6 col-lg-6">
+                <div class="informations">
+                    <h4>Specs</h4>
+                    <hr>
+                    <div class="information">
+                        <dt>Series</dt>
+                        <dd><a href="#">{{$comic->series}}</a></dd>
+                    </div>
+                    <hr>
+                    <div class="information">
+                        <dt>U.S. Price</dt>
+                        <dd>${{$comic->price}}</dd>
+                    </div>
+                    <hr>
+                    <div class="information">
+                        <dt>On Sale Date</dt>
+                        <dd>{{$comic->sale_date}}</dd>
+                    </div>
+                    <hr>
+                    <div class="information">
+                        <dt>Volume/Issue #</dt>
+                        <dd>{{$comic->volume}}</dd>
+                    </div>
+                    <hr>
+                    <div class="information">
+                        <dt>Trim Size</dt>
+                        <dd>{{$comic->trim_size}}</dd>
+                    </div>
+                    <hr>
+                    <div class="information">
+                        <dt>Page Count</dt>
+                        <dd>{{$comic->page_count}}</dd>
+                    </div>
+                    <hr>
+                    <div class="information">
+                        <dt>Rated</dt>
+                        <dd>{{$comic->rated}}</dd>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+
 @endsection
